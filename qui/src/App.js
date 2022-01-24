dc726a2b7a7a50666469c747cf65e938cb6ac03f
Ash <zblash@outlook.com>
2022-01-24 11:24:20 +1100

    feat(layout):build basic layout

--- qui/src/App.js
+++ qui/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Dashboard from './page/dashboard';
+import { Container, Row, Col } from 'react-bootstrap'
+import Header from './component/Header/Header';
+import './user.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container fluid>
+      <Row>
+        <Col xs={{ span: 6, offset: 0 }}>
+          <Header />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={{ span: 10, offset: 1 }}>
+          <Dashboard />
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
